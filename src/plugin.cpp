b15da28cfbe6a24b293b58dac01eae7cc716716f
Bret Truchan <clone45@gmail.com>
2020-01-24 22:59:00 -0800

    work in progress

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -14,6 +14,7 @@ void init(Plugin* p) {
 	p->addModel(modelXY);
 	p->addModel(modelGhosts);
 	p->addModel(modelGoblins);
+	p->addModel(modelAutobreak);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -14,3 +14,4 @@ extern Model *modelWavBank;
 extern Model *modelXY;
 extern Model *modelGhosts;
 extern Model *modelGoblins;
+extern Model *modelAutobreak;
