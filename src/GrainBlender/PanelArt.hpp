cc92324384e2c6c87b2ee671f2f8453eb8368432
Bret Truchan <clone45@gmail.com>
2020-04-26 11:03:32 -0700

    Minor front panel tweaks

--- src/GrainBlender/PanelArt.hpp
+++ src/GrainBlender/PanelArt.hpp
@@ -2,7 +2,7 @@ struct PanelArt : TransparentWidget
 {
 	GrainBlender *module;
 	std::shared_ptr<Font> font;
-  unsigned int warning_fade = 20;
+  unsigned int internal_activity_indicator = 20;
 
 	PanelArt()
 	{
@@ -16,12 +16,12 @@ struct PanelArt : TransparentWidget
 		{
 			if(module->is_spawn_cable_connected == false)
 			{
-        warning_fade += 2;
-        if(warning_fade >= 200) warning_fade = 0;
+        internal_activity_indicator += 2;
+        if(internal_activity_indicator >= 200) internal_activity_indicator = 0;
 
         nvgBeginPath(args.vg);
-        nvgCircle(args.vg, 184.00, 77.00, 15);
-        nvgFillColor(args.vg, nvgRGBA(255, 30, 30, warning_fade));
+        nvgCircle(args.vg, 184.00, 77.00, 13.0 + (float)internal_activity_indicator / 50.0);
+        nvgFillColor(args.vg, nvgRGBA(200, 200, 200, 200));
         nvgFill(args.vg);
 			}
 		}
