7b15d93df3f0f012e004ac9d95634a1faa454df4
Bret Truchan <clone45@gmail.com>
2020-05-27 20:34:40 -0700

    More initial development on GEMK2 expander

--- src/GrainEngineMK2Expander/GrainEngineMK2Expander.hpp
+++ src/GrainEngineMK2Expander/GrainEngineMK2Expander.hpp
@@ -13,9 +13,12 @@ struct GrainEngineMK2Expander : Module
     RECORD_STOP_INPUT,
     AUDIO_IN_LEFT,
     AUDIO_IN_RIGHT,
+    SAMPLE_SLOT_INPUT,
     NUM_INPUTS
   };
   enum OutputIds {
+    PASSTHROUGH_LEFT,
+    PASSTHROUGH_RIGHT,
     NUM_OUTPUTS
   };
   enum LightIds {
@@ -32,6 +35,12 @@ struct GrainEngineMK2Expander : Module
 		if (rightExpander.module && rightExpander.module->model == modelGrainEngineMK2)
     {
 
+      float left = inputs[AUDIO_IN_LEFT].getVoltage();
+      float right = inputs[AUDIO_IN_RIGHT].getVoltage();
+      unsigned int sample_slot = inputs[SAMPLE_SLOT_INPUT].getVoltage() / 2;
+
+      sample_slot = clamp(sample_slot, 0, 4);
+
       if(record_start_trigger.process(inputs[RECORD_START_INPUT].getVoltage()))
       {
         sample.initialize_recording();
@@ -40,28 +49,44 @@ struct GrainEngineMK2Expander : Module
 
       if(recording)
       {
-        float left = inputs[AUDIO_IN_LEFT].getVoltage();
-        float right = inputs[AUDIO_IN_RIGHT].getVoltage();
         sample.record_audio(left, right);
       }
 
       if(record_stop_trigger.process(inputs[RECORD_STOP_INPUT].getVoltage()))
       {
-        std::string path = "test.wav";
+        std::string path = "grain_engine_" + random_string(12) + ".wav";
         sample.save_recorded_audio(path);
         recording = false;
 
-        // Send a message to the GrainEngineMK2 on the right to load the
-        // newly saved .wav file.
+        // Send a message to the GrainEngineMK2 on the right to load the newly saved .wav file.
         GrainEngineExpanderMessage *buffer = (GrainEngineExpanderMessage *) rightExpander.module->leftExpander.producerMessage;
         buffer->path = path;
-        buffer->sample_slot = 0;
+        buffer->sample_slot = sample_slot;
         buffer->message_received = false;
       }
+
+      outputs[PASSTHROUGH_LEFT].setVoltage(left);
+      outputs[PASSTHROUGH_RIGHT].setVoltage(right);
 		}
 		else
     {
 			// No Grain Engine MK2 to the right, so do nothing
 		}
 	}
+
+  std::string random_string( size_t length )
+  {
+      auto randchar = []() -> char
+      {
+          const char charset[] =
+          "0123456789"
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+          "abcdefghijklmnopqrstuvwxyz";
+          const size_t max_index = (sizeof(charset) - 1);
+          return charset[ rand() % max_index ];
+      };
+      std::string str(length,0);
+      std::generate_n( str.begin(), length, randchar );
+      return str;
+  }
 };
--- src/GrainEngineMK2Expander/GrainEngineMK2ExpanderWidget.hpp
+++ src/GrainEngineMK2Expander/GrainEngineMK2ExpanderWidget.hpp
@@ -8,9 +8,13 @@ struct GrainEngineMK2ExpanderWidget : ModuleWidget
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20, 50)), module, GrainEngineMK2Expander::RECORD_START_INPUT));
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20, 60)), module, GrainEngineMK2Expander::RECORD_STOP_INPUT));
 
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20, 76)), module, GrainEngineMK2Expander::SAMPLE_SLOT_INPUT));
+
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20, 30)), module, GrainEngineMK2Expander::AUDIO_IN_LEFT));
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(30, 30)), module, GrainEngineMK2Expander::AUDIO_IN_RIGHT));
 
+    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(20, 100)), module, GrainEngineMK2Expander::PASSTHROUGH_LEFT));
+    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(30, 100)), module, GrainEngineMK2Expander::PASSTHROUGH_RIGHT));
   }
 
   void appendContextMenu(Menu *menu) override
