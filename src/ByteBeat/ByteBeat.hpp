4e8996ce36ff22ff4443c96f98777235ad0355ad
Bret Truchan <clone45@gmail.com>
2020-11-14 14:18:26 -0800

    Work in progress

--- src/ByteBeat/ByteBeat.hpp
+++ src/ByteBeat/ByteBeat.hpp
@@ -3,18 +3,26 @@ struct ByteBeat : Module
   float left_audio = 0;
   float right_audio = 0;
 
-  uint32_t t; // 32 bits
-  uint8_t w;
+  uint32_t t;  // t is our time counter used in the equations
+  uint32_t p1; // p1-p3 are variables used (optionally) in equations
+  uint32_t p2;
+  uint32_t p3;
+  uint8_t w;   // w is the output of the equation
+  uint8_t clock_division_counter = 0;
+  uint8_t clock_division = 2;
 
   enum ParamIds {
+    CLOCK_DIVISION_KNOB,
 		NUM_PARAMS
 	};
 	enum InputIds {
+    PARAM_INPUT_1,
+    PARAM_INPUT_2,
+    PARAM_INPUT_3,
 		NUM_INPUTS
 	};
 	enum OutputIds {
-    AUDIO_OUTPUT_LEFT,
-    AUDIO_OUTPUT_RIGHT,
+    AUDIO_OUTPUT,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
@@ -24,6 +32,8 @@ struct ByteBeat : Module
 	ByteBeat()
 	{
     config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+
+    configParam(CLOCK_DIVISION_KNOB, 0.0f, 16.0f, 0.0f, "ClockDivisionKnob");
 	}
 
 	// Autosave module data.  VCV Rack decides when this should be called.
@@ -40,20 +50,61 @@ struct ByteBeat : Module
 
 	void process(const ProcessArgs &args) override
 	{
+    clock_division_counter ++;
+    if(clock_division_counter >= clock_division)
+    {
+      t = t + 1;
+      clock_division_counter = 0;
+    }
 
-    t = t + 1;
+    p1 = (inputs[PARAM_INPUT_1].getVoltage() / 10.0) * 4096.0;
+    p2 = (inputs[PARAM_INPUT_2].getVoltage() / 10.0) * 4096.0;
+    p3 = (inputs[PARAM_INPUT_3].getVoltage() / 10.0) * 4096.0;
 
-    float output = compute(t, 0, 0, 0);
+    clock_division = params[CLOCK_DIVISION_KNOB].getValue(); // float to int conversion happening here
 
-    outputs[AUDIO_OUTPUT_LEFT].setVoltage(output);
-    // outputs[AUDIO_MIX_OUTPUT_RIGHT].setVoltage(summed_output_right);
+    float output = compute(t, p1, p2, p3);
+
+    outputs[AUDIO_OUTPUT].setVoltage(output);
   }
 
   float compute(uint32_t t, uint32_t p1, uint32_t p2, uint32_t p3)
   {
-    // w = (((t/(p3+1)*t)%p1)&(t-t/p2*632));
-    // Try: (t>>13&t)*(t>>8)
-    w = (uint8_t) ((t>>13&t)*(t>>8));
+    // >> has precidence over mod
+
+    // w = (uint8_t) ((t>>(13 + p1)&t)*(t>>(p2)));
+    // w = div(((t^(p1>>3)-456)*(p2+1)),(((t>>(p3>>3))%14)+1))+(t*((182>>(t>>15)%16))&1);
+
+    // Alpha
+    // w = div(((t^(p1>>3)-456)*(p2+1)),(((t>>(p3>>3))%14)+1))+(t*((182>>(t>>15)%16))&1);
+
+    // Omega
+    // w= (((t>>5)|(t<<((p3>>4)+1))>>4)*p1-(div(t,((1853>>(t>>(p2>>4))%15)))&1)>>(t>>12)%6)>>4;
+
+    // Widerange
+    // w = (((p1^(t>>(p2>>3)))-(t>>(p3>>2))-mod(t,(t&p2))));
+
+    // Defender
+    // w = ( mod((w-482+(w^(3456-p1))), (p2*t>>5&(1030-(p3<<2)))) );
+
+    // Maginfied
+    // w = (mod((p1 & t), p3)) ^ div(t,p2);
+
+    // Light Reactor
+    w = mod((p1+t>>p2),12)|((  mod(w,(p1+t>>p1%4)))+11+p3^t)>>(p3>>12);
+
     return(w / 256.0);
+  }
+
+  uint32_t div(uint32_t a, uint32_t b)
+  {
+    if(b == 0) return(0);
+    return(a / b);
+  }
+
+  uint32_t mod(uint32_t a, uint32_t b)
+  {
+    if(b == 0) return(0);
+    return(a % b);
   }
 };
--- src/ByteBeat/ByteBeatWidget.hpp
+++ src/ByteBeat/ByteBeatWidget.hpp
@@ -5,13 +5,25 @@ struct ByteBeatWidget : ModuleWidget
     float group_y = 30;
     float row_padding = 10.2;
 
+    float triggers_column_x = 9.5;
+    float position_inputs_column_x = 10.5 + 10.16;
+    float volume_knobs_column_x = 20.5 + 10.16;
+    float pan_knobs_column_x = 30.5 + 10.16;
+
     float output_l_column = 29.08 + 15.24 + 10.16;
     float output_r_column = 39.78 + 15.24 + 10.16;
 
     setModule(module);
     setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/bytebeat_front_panel.svg")));
 
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(output_l_column, group_y + (row_padding * 0))), module, ByteBeat::AUDIO_OUTPUT_LEFT));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(position_inputs_column_x, group_y + (row_padding * 0))), module, ByteBeat::PARAM_INPUT_1));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(position_inputs_column_x, group_y + (row_padding * 1))), module, ByteBeat::PARAM_INPUT_2));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(position_inputs_column_x, group_y + (row_padding * 2))), module, ByteBeat::PARAM_INPUT_3));
+
+    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(output_l_column, group_y + (row_padding * 0))), module, ByteBeat::AUDIO_OUTPUT));
+
+    addParam(createParamCentered<Trimpot>(mm2px(Vec(volume_knobs_column_x, group_y + (row_padding * 0))), module, ByteBeat::CLOCK_DIVISION_KNOB));
+
 
   }
 };
