e8583888ba825d3f451d1d62092f539e25a93a53
Bret Truchan <clone45@gmail.com>
2021-01-03 13:35:21 -0800

    Cleaned up front panel and added more comments

--- src/ByteBeat/ByteBeatWidget.hpp
+++ src/ByteBeat/ByteBeatWidget.hpp
@@ -15,15 +15,16 @@ struct ByteBeatWidget : ModuleWidget
 
     // Parameter inputs
 
-    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_8, ROW_8)), module, ByteBeat::PARAM_KNOB_1));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_8, ROW_10)), module, ByteBeat::PARAM_INPUT_1));
+    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_8, ROW_3)), module, ByteBeat::PARAM_KNOB_1));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_8, ROW_5)), module, ByteBeat::PARAM_INPUT_1));
 
-    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_11, ROW_8)), module, ByteBeat::PARAM_KNOB_2));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_11, ROW_10)), module, ByteBeat::PARAM_INPUT_2));
+    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_11, ROW_3)), module, ByteBeat::PARAM_KNOB_2));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_11, ROW_5)), module, ByteBeat::PARAM_INPUT_2));
 
-    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_14, ROW_8)), module, ByteBeat::PARAM_KNOB_3));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_14, ROW_10)), module, ByteBeat::PARAM_INPUT_3));
+    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_14, ROW_3)), module, ByteBeat::PARAM_KNOB_3));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_14, ROW_5)), module, ByteBeat::PARAM_INPUT_3));
 
+    // Other
     addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(COLUMN_14, ROW_14)), module, ByteBeat::AUDIO_OUTPUT));
     // addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(COLUMN_12, ROW_14)), module, ByteBeat::DEBUG_OUTPUT));
 
