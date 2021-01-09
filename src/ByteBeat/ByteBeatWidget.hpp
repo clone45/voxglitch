470f210f899744dda219690327f3bdfdbdf7bd1e
Bret Truchan <clone45@gmail.com>
2021-01-09 13:13:44 -0800

    Bytebeat (#70)
    Added additional equation and made huge improvements to how the parameter inputs behave.

--- src/ByteBeat/ByteBeatWidget.hpp
+++ src/ByteBeat/ByteBeatWidget.hpp
@@ -9,21 +9,31 @@ struct ByteBeatWidget : ModuleWidget
 
     // addParam(createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(COLUMN_3, ROW_3 AND_A_HALF_ROW)), module, ByteBeat::EQUATION_KNOB));
     auto L1 = createParamCentered<RoundHugeBlackKnob>(mm2px(Vec(COLUMN_3, ROW_3 AND_A_HALF_ROW)), module, ByteBeat::EQUATION_KNOB); dynamic_cast<Knob*>(L1)->snap = true; addParam(L1);
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_5, ROW_5)), module, ByteBeat::EQUATION_INPUT));
 
     // Parameter inputs
-    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_8, ROW_3)), module, ByteBeat::PARAM_KNOB_1));
+    auto P1 = createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_8, ROW_3)), module, ByteBeat::PARAM_KNOB_1); dynamic_cast<Knob*>(P1)->snap = true; addParam(P1);
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_8, ROW_5)), module, ByteBeat::PARAM_INPUT_1));
 
-    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_11, ROW_3)), module, ByteBeat::PARAM_KNOB_2));
+    auto P2 = createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_11, ROW_3)), module, ByteBeat::PARAM_KNOB_2); dynamic_cast<Knob*>(P2)->snap = true; addParam(P2);
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_11, ROW_5)), module, ByteBeat::PARAM_INPUT_2));
 
-    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_14, ROW_3)), module, ByteBeat::PARAM_KNOB_3));
+    auto P3 = createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_14, ROW_3)), module, ByteBeat::PARAM_KNOB_3); dynamic_cast<Knob*>(P3)->snap = true; addParam(P3);
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_14, ROW_5)), module, ByteBeat::PARAM_INPUT_3));
 
     // Other
     addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(COLUMN_14, ROW_14)), module, ByteBeat::AUDIO_OUTPUT));
-    // addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(COLUMN_12, ROW_14)), module, ByteBeat::DEBUG_OUTPUT));
 
     addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(COLUMN_3, ROW_7)), module, ByteBeat::CLOCK_DIVISION_KNOB));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(COLUMN_5, ROW_8)), module, ByteBeat::CLOCK_CV_INPUT));
   }
+
+  /*
+  void add_snapping_parameter_knob(float column, float row, int index)
+  {
+    auto P = createParamCentered<RoundBlackKnob>(mm2px(Vec(column, row)), module, index);
+    dynamic_cast<Knob*>(P)->snap = true;
+    addParam(P);
+  }
+  */
 };
