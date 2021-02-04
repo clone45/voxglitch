e7770e59360492e3796bd2e66136f072c32a035f
Bret Truchan <clone45@gmail.com>
2021-02-03 17:55:51 -0800

    Hazumi (#72)
    * Added options for triggering on bottom, top, or both.  Also added a flash when a ball triggers an output.

--- src/Hazumi/HazumiSequencer.hpp
+++ src/Hazumi/HazumiSequencer.hpp
@@ -1,19 +1,16 @@
 struct HazumiSequencer
 {
+  unsigned int trigger_options[SEQUENCER_COLUMNS] = {0,0,0,0,0,0,0,0};
+
   // Start the balls at the bottom
-  unsigned int ball_locations[8] = {
-    0,0,0,0,0,0,0,0
-  };
+  unsigned int ball_locations[8] = { 0,0,0,0,0,0,0,0 };
 
-  // 0 == down
-  // 1 == up
-  bool ball_directions[8] = {
-    1,1,1,1,1,1,1,1
-  };
+  bool ball_directions[8] = { 1,1,1,1,1,1,1,1 }; // 0 == down, 1 == up
+  unsigned int column_heights[SEQUENCER_COLUMNS] = { 1,1,1,1,1,1,1,1 };
 
-  unsigned int column_heights[8] = {
-    1,1,1,1,1,1,1,1
-  };
+  // Store the trigger results in the sequencer.  This will be used by
+  // the widget to color the balls differently when they trigger an output
+  bool stored_trigger_results[SEQUENCER_COLUMNS] = { 0,0,0,0,0,0,0,0 };
 
   // constructor
   HazumiSequencer()
@@ -25,8 +22,15 @@ struct HazumiSequencer
 
   void step(bool *trigger_results)
   {
+    bool hit_top = false;
+    bool hit_bottom = false;
+
     for(unsigned int i=0; i<8; i++)
     {
+      hit_top = false;
+      hit_bottom = false;
+      trigger_results[i] = false;
+
       if(column_heights[i] == 1)
       {
         ball_directions[i] = 1;
@@ -56,17 +60,18 @@ struct HazumiSequencer
         ball_locations[i] += 1;
       }
 
-      // After the ball has been moved, see if it's at the bottom of the
-      // sequencer.  If so, set the corresponding trigger result.  If not,
-      // clear the corresponding trigger result.
-      if(ball_locations[i] == 0 && column_heights[i] != 1)
-      {
-        trigger_results[i] = true;
-      }
-      else
-      {
-        trigger_results[i] = false;
-      }
+      //
+      // After the ball has been moved, see if it should trigger an output
+      //
+
+      if(ball_locations[i] == 0 && column_heights[i] != 1) hit_bottom = true;
+      if((ball_locations[i] > 0) && (ball_locations[i] == (column_heights[i] - 1))) hit_top = true;
+
+      if(trigger_options[i] == TRIGGER_AT_BOTTOM && hit_bottom) trigger_results[i] = true;
+      if(trigger_options[i] == TRIGGER_AT_TOP && hit_top) trigger_results[i] = true;
+      if(trigger_options[i] == TRIGGER_AT_BOTH && (hit_bottom || hit_top)) trigger_results[i] = true;
+
+      stored_trigger_results[i] = trigger_results[i];
     }
   }
 
