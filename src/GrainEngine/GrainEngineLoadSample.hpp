f430c1dd2a94e0dff00141371467f372613f2116
falkTX <falktx@falktx.com>
2022-03-07 16:17:11 +0000

    Cardinal specific changes
    Signed-off-by: falkTX <falktx@falktx.com>

--- src/GrainEngine/GrainEngineLoadSample.hpp
+++ src/GrainEngine/GrainEngineLoadSample.hpp
@@ -5,9 +5,20 @@ struct GrainEngineLoadSample : MenuItem
 	void onAction(const event::Action &e) override
 	{
 		const std::string dir = "";
+#ifdef USING_CARDINAL_NOT_RACK
+		GrainEngine *module = this->module;
+		async_dialog_filebrowser(false, dir.c_str(), text.c_str(), [module](char* path) {
+			pathSelected(module, path);
+		});
+#else
 		char *path = osdialog_file(OSDIALOG_OPEN, dir.c_str(), NULL, osdialog_filters_parse("Wav:wav"));
+		pathSelected(module, path);
+#endif
+	}
 
-		if(path)
+	static void pathSelected(GrainEngine *module, char *path)
+	{
+		if (path)
 		{
 			module->sample.load(path);
 			module->root_dir = std::string(path);
--- src/GrainEngineMK2/GrainEngineMK2LoadSample.hpp
+++ src/GrainEngineMK2/GrainEngineMK2LoadSample.hpp
@@ -1,17 +1,29 @@
 struct GrainEngineMK2LoadSample : MenuItem
 {
 	GrainEngineMK2 *module;
-  unsigned int sample_number = 0;
+	unsigned int sample_number = 0;
 
 	void onAction(const event::Action &e) override
 	{
 		const std::string dir = module->root_dir.empty() ? "" : module->root_dir;
+#ifdef USING_CARDINAL_NOT_RACK
+		GrainEngineMK2 *module = this->module;
+		unsigned int sample_number = this->sample_number;
+		async_dialog_filebrowser(false, dir.c_str(), text.c_str(), [module, sample_number](char* path) {
+			pathSelected(module, sample_number, path);
+		});
+#else
 		char *path = osdialog_file(OSDIALOG_OPEN, dir.c_str(), NULL, osdialog_filters_parse("Wav:wav"));
+		pathSelected(module, sample_number, path);
+#endif
+	}
 
-		if(path)
+	static void pathSelected(GrainEngineMK2 *module, unsigned int sample_number, char *path)
+	{
+		if (path)
 		{
-      module->load_queue.queue_sample_for_loading(std::string(path), sample_number);
-      module->fade_out_on_load.trigger();
+			module->load_queue.queue_sample_for_loading(std::string(path), sample_number);
+			module->fade_out_on_load.trigger();
 
 			// module->samples[sample_number]->load(path);
 			// module->root_dir = std::string(path);
