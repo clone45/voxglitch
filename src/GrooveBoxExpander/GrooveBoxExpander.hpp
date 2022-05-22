d8588df173207a0d6759faece364bcde8e108d49
Bret Truchan <clone45@gmail.com>
2022-05-21 17:09:57 -0700

    Fixed issues where expander would not necessarily communicate status when patch is first loaded

--- src/GrooveBoxExpander/GrooveBoxExpander.hpp
+++ src/GrooveBoxExpander/GrooveBoxExpander.hpp
@@ -39,6 +39,66 @@ struct GrooveBoxExpander : Module
     NUM_LIGHTS
   };
 
+  json_t *dataToJson() override
+	{
+    json_t *json_root = json_object();
+
+    //
+    // Save mutes
+    //
+
+    json_t *mutes_json_array = json_array();
+    for(int i=0; i < NUMBER_OF_TRACKS; i++)
+    {
+      json_array_append_new(mutes_json_array, json_integer(this->mutes[i]));
+    }
+    json_object_set(json_root, "mutes", mutes_json_array);
+    json_decref(mutes_json_array);
+
+    //
+    // Save solos
+    //
+
+    json_t *solos_json_array = json_array();
+    for(int i=0; i < NUMBER_OF_TRACKS; i++)
+    {
+      json_array_append_new(solos_json_array, json_integer(this->solos[i]));
+    }
+    json_object_set(json_root, "solos", solos_json_array);
+    json_decref(solos_json_array);
+
+    return json_root;
+  }
+
+  void dataFromJson(json_t *json_root) override
+	{
+    // Load mutes
+    json_t *mutes_json_object = json_object_get(json_root, "mutes");
+    if(mutes_json_object)
+    {
+      size_t i;
+      json_t *mutes_json = NULL;
+      json_array_foreach(mutes_json_object, i, mutes_json)
+      {
+        this->mutes[i] = json_integer_value(mutes_json);
+      }
+    }
+
+    // Load solos
+    json_t *solos_json_object = json_object_get(json_root, "solos");
+    if(solos_json_object)
+    {
+      size_t i;
+      json_t *solos_json = NULL;
+      json_array_foreach(solos_json_object, i, solos_json)
+      {
+        this->solos[i] = json_integer_value(solos_json);
+      }
+    }
+
+    send_update_to_groovebox = true;
+  }
+
   // Constructor
   GrooveBoxExpander()
   {
@@ -67,67 +127,41 @@ struct GrooveBoxExpander : Module
     for(unsigned int i=0; i < NUMBER_OF_TRACKS; i++)
     {
       //
-      // Read mute buttons abd inputs
+      // Read mute buttons and inputs
       //
-
       bool mute_button_pressed = mute_button_triggers[i].process(params[MUTE_BUTTONS + i].getValue());
       bool mute_button_triggered = mute_cv_triggers[i].process(rescale(inputs[MUTE_INPUTS + i].getVoltage(), 0.0f, 10.0f, 0.f, 1.f));
-
-      if(mute_button_pressed || mute_button_triggered) {
-         mutes[i] = !mutes[i];
-         send_update_to_groovebox = true;
-      }
+      if(mute_button_pressed || mute_button_triggered) mutes[i] = !mutes[i];
 
       //
       // Read solo buttons and inputs
       //
-
       bool solo_button_pressed = solo_button_triggers[i].process(params[SOLO_BUTTONS + i].getValue());
       bool solo_button_triggered = solo_cv_triggers[i].process(rescale(inputs[SOLO_INPUTS + i].getVoltage(), 0.0f, 10.0f, 0.f, 1.f));
-
-      if(solo_button_pressed || solo_button_triggered) {
-         solos[i] = !solos[i];
-         send_update_to_groovebox = true;
-      }
+      if(solo_button_pressed || solo_button_triggered) solos[i] = !solos[i];
 
       //
       // See if track volumes have changed
       //
 
       float new_track_volume = params[VOLUME_KNOBS + i].getValue();
-      if(old_track_volumes[i] != new_track_volume)
-      {
-        old_track_volumes[i] = new_track_volume;
-        send_update_to_groovebox = true;
-      }
+      if(old_track_volumes[i] != new_track_volume) old_track_volumes[i] = new_track_volume;
 
       // Set mute and solo lights
       lights[MUTE_BUTTON_LIGHTS + i].setBrightness(mutes[i]);
       lights[SOLO_BUTTON_LIGHTS + i].setBrightness(solos[i]);
     }
 
-    if(rightExpander.module && rightExpander.module->model == modelGrooveBox)
-    {
-      // If the expander was just added or not yet processed (such as when
-      // the patch first loads), then send the info to the groovebox
-      if(expander_connected == false) send_update_to_groovebox = true;
-      expander_connected = true;
-    }
-    else
-    {
-      expander_connected = false;
-    }
+    expander_connected = (rightExpander.module && rightExpander.module->model == modelGrooveBox);
 
     //
     // Send information to groovebox
     //
-    if (send_update_to_groovebox)
+    if (expander_connected)
     {
       writeToGroovebox();
-      send_update_to_groovebox = false;
+      readFromGroovebox();
     }
-
-    readFromGroovebox();
 
     // This is required after the flip
     // rightExpander.producerMessage = producer_message;
@@ -145,52 +179,46 @@ struct GrooveBoxExpander : Module
 
   void writeToGroovebox()
   {
-    if (rightExpander.module && rightExpander.module->model == modelGrooveBox)
+    // Prepare message for sending to Grain Engine MK2
+    // When writing to the groovebox, we're using the __GrooveBox's__ producer and consumer pair
+    // Always write to the producer and read from the consumer
+    ExpanderToGrooveboxMessage *message_to_groove_box = (ExpanderToGrooveboxMessage *) rightExpander.module->leftExpander.producerMessage;
+
+    // Wait until the groovebox received the last message
+    if(message_to_groove_box && message_to_groove_box->message_received == true)
     {
-      // Prepare message for sending to Grain Engine MK2
-      // When writing to the groovebox, we're using the __GrooveBox's__ producer and consumer pair
-      // Always write to the producer and read from the consumer
-      ExpanderToGrooveboxMessage *message_to_groove_box = (ExpanderToGrooveboxMessage *) rightExpander.module->leftExpander.producerMessage;
-
-      // Wait until the groovebox received the last message
-      if(message_to_groove_box && message_to_groove_box->message_received == true)
+      for(unsigned int i=0; i < NUMBER_OF_TRACKS; i++)
       {
-        for(unsigned int i=0; i < NUMBER_OF_TRACKS; i++)
-        {
-          message_to_groove_box->mutes[i] = mutes[i];
-          message_to_groove_box->solos[i] = solos[i];
-          message_to_groove_box->track_volumes[i] = params[VOLUME_KNOBS + i].getValue();
-        }
-
-        // Tell GrooveBox that the message is ready for receiving
-        message_to_groove_box->message_received = false;
+        message_to_groove_box->mutes[i] = mutes[i];
+        message_to_groove_box->solos[i] = solos[i];
+        message_to_groove_box->track_volumes[i] = params[VOLUME_KNOBS + i].getValue();
       }
+
+      // Tell GrooveBox that the message is ready for receiving
+      message_to_groove_box->message_received = false;
     }
   }
 
   void readFromGroovebox()
   {
-    if (rightExpander.module && rightExpander.module->model == modelGrooveBox)
+    // Receive message from expander
+    GrooveboxToExpanderMessage *groovebox_message = (GrooveboxToExpanderMessage *) this->rightExpander.consumerMessage;
+
+    // Retrieve the data from the expander
+    if(groovebox_message && groovebox_message->message_received == false)
     {
-      // Receive message from expander
-      GrooveboxToExpanderMessage *groovebox_message = (GrooveboxToExpanderMessage *) this->rightExpander.consumerMessage;
-
-      // Retrieve the data from the expander
-      if(groovebox_message && groovebox_message->message_received == false)
+      for(unsigned int i=0; i < NUMBER_OF_TRACKS; i++)
       {
-        for(unsigned int i=0; i < NUMBER_OF_TRACKS; i++)
+        if(groovebox_message->track_triggers[i])
         {
-          if(groovebox_message->track_triggers[i])
-          {
-            // Trigger output
-            triggerOutputPulseGenerators[i].trigger(0.01f);
-            triggerLightPulseGenerators[i].trigger(0.05f);
-          }
+          // Trigger output
+          triggerOutputPulseGenerators[i].trigger(0.01f);
+          triggerLightPulseGenerators[i].trigger(0.05f);
         }
-
-        groovebox_message->message_received = true;
       }
-      rightExpander.messageFlipRequested = true;
+
+      groovebox_message->message_received = true;
     }
+    rightExpander.messageFlipRequested = true;
   }
 };
