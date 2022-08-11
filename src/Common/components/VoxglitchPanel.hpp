50cc282715c1b879ab4076b9faacc505acbc66a1
Bret Truchan <clone45@gmail.com>
2022-08-11 10:14:44 -0700

    Fixed location of gate sequence in digital sequencer

--- src/Common/components/VoxglitchPanel.hpp
+++ src/Common/components/VoxglitchPanel.hpp
@@ -2,10 +2,6 @@ struct VoxglitchPanel : app::SvgPanel {
 
   VoxglitchPanel()
   {
-#if defined DEV_MODE
-  panelBorder->visible = true;
-#else
-  panelBorder->visible = false;
-#endif
+    panelBorder->visible = true;
   }
 };
