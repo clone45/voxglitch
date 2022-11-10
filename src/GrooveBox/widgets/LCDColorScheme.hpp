901a4b5dd203116284691522a60879ef71e09b00
Jim Allman <jim@ibang.com>
2022-11-10 11:26:08 -0500

    Add LCD color schemes and switching UI

--- src/GrooveBox/widgets/LCDColorScheme.hpp
+++ src/GrooveBox/widgets/LCDColorScheme.hpp
@@ -2,32 +2,47 @@ struct LCDColorScheme
 {
     unsigned int selected_color_scheme = 0;
 
-    NVGcolor color_scheme[3][5] = {
-        // Vampire Red
+    NVGcolor color_scheme[5][5] = {
+        // Legacy Red
         {
-            nvgRGB(60,110,100),
-            nvgRGBA(146, 42, 43, 140),    // dark color
-            nvgRGBA(245, 141, 138, 140),  // light color
-            nvgRGBA(255, 255, 255, 50),   // normal highlight overlay
-            nvgRGBA(255, 255, 255, 100),   // strong highlight overlay
-            
+            nvgRGB(50,0,0),               // panel background
+            nvgRGBA(128, 35, 35, 140),    // dark blocks
+            nvgRGBA(200, 50, 50, 140),  // light blocks
+            nvgRGBA(255, 0, 0, 50),       // normal highlight overlay
+            nvgRGBA(255, 0, 0, 100)       // strong highlight overlay
         },
-        // Theme #2
+        // Soft Yellow
         {
-            nvgRGB(60,80,120),
-            nvgRGBA(46, 142, 43, 140),    // dark color
-            nvgRGBA(145, 141, 138, 140),  // light color
+            nvgRGB(40, 25, 0),             // panel background
+            nvgRGBA(110, 90, 0, 140),    // dark blocks
+            nvgRGBA(160, 140, 0, 140),  // light blocks
+            nvgRGBA(220, 200, 0, 50),     // normal highlight overlay
+            nvgRGBA(220, 200, 0, 100)     // strong highlight overlay
+        },
+        // Cool White
+        {
+            nvgRGB(10, 10, 10),             // panel background
+            nvgRGBA(70, 70, 70, 140),       // dark blocks
+            nvgRGBA(135, 135, 135, 140),      // light blocks
             nvgRGBA(255, 255, 255, 50),   // normal highlight overlay
             nvgRGBA(255, 255, 255, 100)   // strong highlight overlay
         },
-        // Theme #3
+        // Ice Blue
         {
-            nvgRGB(60,0,1),
-            nvgRGBA(146, 42, 43, 140),    // dark color
-            nvgRGBA(245, 141, 138, 140),  // light color
-            nvgRGBA(255, 255, 255, 50),   // normal highlight overlay
-            nvgRGBA(255, 255, 255, 100)   // strong highlight overlay
-        }                
+            nvgRGB(0, 30, 45),             // panel bqackground
+            nvgRGBA(35, 95, 125, 140),       // dark blocks
+            nvgRGBA(50, 150, 210, 140),      // light blocks
+            nvgRGBA(0, 180, 255, 50),   // normal highlight overlay
+            nvgRGBA(0, 180, 255, 100)   // strong highlight overlay
+        },
+        // Data Green
+        {
+            nvgRGB(0, 50, 0),               // panel background
+            nvgRGBA(35, 128, 35, 140),    // dark blocks
+            nvgRGBA(50, 185, 50, 140),   // light blocks
+            nvgRGBA(0, 255, 0, 50),   // normal highlight overlay
+            nvgRGBA(0, 255, 0, 100)   // strong highlight overlay
+        }
     };
 
     NVGcolor getBackgroundColor()
