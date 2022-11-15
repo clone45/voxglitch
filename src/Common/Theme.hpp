7ed59832de14f3d8de22619bbb08ddd088007d84
Bret Truchan <clone45@gmail.com>
2022-11-15 15:50:24 -0800

    Updated code to fallback on the correct theme name if the Voxglitch.json file could not be read or parsed

--- src/Common/Theme.hpp
+++ src/Common/Theme.hpp
@@ -1,6 +1,6 @@
 struct Theme
 {
-  std::string name = "dark";
+  std::string name = "default";
   json_t *json_root = NULL;
   json_t *widgets = NULL;
   bool show_screws = true;
@@ -17,13 +17,20 @@ struct Theme
 
     std::string config_file_path = asset::user("Voxglitch.json");
 
-    // Load theme selection, either "light" or "dark"
-    json_root_local = json_load_file(config_file_path.c_str(), 0, &error);
-    if(json_root_local)
+    // Load theme selection, either "light" or "default"
+    if(rack::system::exists(config_file_path.c_str()))
     {
-      json_t* theme_json = json_object_get(json_root_local, "theme");
-      if (theme_json) name = json_string_value(theme_json);
-      json_decref(json_root_local);
+      json_root_local = json_load_file(config_file_path.c_str(), 0, &error);
+      if(json_root_local)
+      {
+        json_t* theme_json = json_object_get(json_root_local, "theme");
+        if (theme_json) name = json_string_value(theme_json);
+        json_decref(json_root_local);
+      }
+    }
+    else
+    {
+      name = "default";
     }
   }
 
