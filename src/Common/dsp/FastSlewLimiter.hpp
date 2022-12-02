15bf7889d3d16d5fc660c03d0b94641114c57934
Bret Truchan <clone45@gmail.com>
2022-12-02 08:23:06 -0800

    fixed formatting on fast slew limiter code

--- src/Common/dsp/FastSlewLimiter.hpp
+++ src/Common/dsp/FastSlewLimiter.hpp
@@ -1,38 +1,44 @@
 /** Limits the derivative of the output by a rise and fall speed, in units/s. */
 template <typename T = float>
-struct TFastSlewLimiter {
-	T out = 0.f;
-	T rise = 0.f;
-	T fall = 0.f;
+struct TFastSlewLimiter
+{
+    T out = 0.f;
+    T rise = 0.f;
+    T fall = 0.f;
     T deltaTime = 0.f;
     T fallDelta = 0.f;
     T riseDelta = 0.f;
 
-	void reset() {
-		out = 0.f;
-	}
+    void reset()
+    {
+        out = 0.f;
+    }
 
-	void setRiseFall(T rise, T fall) {
-		this->rise = rise;
-		this->fall = fall;
-	}
+    void setRiseFall(T rise, T fall)
+    {
+        this->rise = rise;
+        this->fall = fall;
+    }
 
-	void setDeltaTime(T deltaTime) {
-		this->deltaTime = deltaTime;
+    void setDeltaTime(T deltaTime)
+    {
+        this->deltaTime = deltaTime;
         this->fallDelta = fall * deltaTime;
         this->riseDelta = rise * deltaTime;
-	}
+    }
 
-	T process(T in) {
-        if(in == out) return out;
-		out = simd::clamp(in, out - fallDelta, out + riseDelta);
-		return out;
-	}
+    T process(T in)
+    {
+        if (in == out)
+            return out;
+        out = simd::clamp(in, out - fallDelta, out + riseDelta);
+        return out;
+    }
 
     void updateRackSampleRate()
     {
-      setDeltaTime(APP->engine->getSampleTime());
-    }    
+        setDeltaTime(APP->engine->getSampleTime());
+    }
 };
 
 typedef TFastSlewLimiter<> FastSlewLimiter;
