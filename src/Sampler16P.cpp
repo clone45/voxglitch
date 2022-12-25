8f33d0e1d9b418e17b43ee7dad3374e5ac64647f
Bret Truchan <clone45@gmail.com>
2022-12-24 16:26:59 -0800

    Created a slim, 16 POLY sample player (#208)
    * Created a slim, 16 POLY sample player
    
    * Fixed bug with filename caps mismatch

---
+++ src/Sampler16P.cpp
@@ -0,0 +1,20 @@
+//
+// Voxglitch "SamplerX8" module for VCV Rack
+//
+
+#include "plugin.hpp"
+#include "osdialog.h"
+
+#include "Common/sample.hpp"
+#include "Common/Theme.hpp"
+#include "Common/components/VoxglitchComponents.hpp"
+#include "Common/SamplePlayer.hpp"
+#include "Common/dsp/StereoPan.hpp"
+
+#include "Sampler16P/defines.h"
+#include "Sampler16P/Sampler16P.hpp"
+#include "Sampler16P/Sampler16PLoadSample.hpp"
+#include "Sampler16P/Sampler16PLoadFolder.hpp"
+#include "Sampler16P/Sampler16PWidget.hpp"
+
+Model* modelSampler16P = createModel<Sampler16P, Sampler16PWidget>("sampler16p");
