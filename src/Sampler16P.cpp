ef9ebfee7437776b80fbaa6b74b79fb1eb53bba6
Bret Truchan <clone45@gmail.com>
2022-12-24 16:04:02 -0800

    Fixed bug with filename caps mismatch

--- src/Sampler16P.cpp
+++ src/Sampler16P.cpp
@@ -11,10 +11,10 @@
 #include "Common/SamplePlayer.hpp"
 #include "Common/dsp/StereoPan.hpp"
 
-#include "Sampler16p/defines.h"
-#include "Sampler16p/Sampler16P.hpp"
-#include "Sampler16p/Sampler16PLoadSample.hpp"
-#include "Sampler16p/Sampler16PLoadFolder.hpp"
-#include "Sampler16p/Sampler16PWidget.hpp"
+#include "Sampler16P/defines.h"
+#include "Sampler16P/Sampler16P.hpp"
+#include "Sampler16P/Sampler16PLoadSample.hpp"
+#include "Sampler16P/Sampler16PLoadFolder.hpp"
+#include "Sampler16P/Sampler16PWidget.hpp"
 
 Model* modelSampler16P = createModel<Sampler16P, Sampler16PWidget>("sampler16p");
