e3a2a10ffbc231c036dba28e306ef50c9d6d1e99
Bret Truchan <clone45@gmail.com>
2023-01-13 17:20:58 -0800

    Fixed bug in Grain Engine MK2 where samples were loading into incorrect slots

--- src/GrainEngineMK2/GrainEngineMK2LoadSample.hpp
+++ src/GrainEngineMK2/GrainEngineMK2LoadSample.hpp
@@ -9,15 +9,15 @@ struct GrainEngineMK2LoadSample : MenuItem
 #ifdef USING_CARDINAL_NOT_RACK
 		GrainEngineMK2 *module = this->module;
 		unsigned int sample_number = this->sample_number;
-		async_dialog_filebrowser(false, NULL, dir.c_str(), "Load sample", [module, sample_number](char* filename) {
+		async_dialog_filebrowser(false, NULL, dir.c_str(), "Load sample", [module, sample_number](char *filename)
+								 {
       if(filename)
       {
         fileSelected(module, sample_number, std::string(filename));
         free(filename);
-      }
-		});
+      } });
 #else
-    fileSelected(module, this->sample_number, module->selectFileVCV());
+		fileSelected(module, this->sample_number, module->selectFileVCV());
 #endif
 	}
 
@@ -28,10 +28,16 @@ struct GrainEngineMK2LoadSample : MenuItem
 			// module->load_queue.queue_sample_for_loading(filename, sample_number);
 			// module->stereo_fade_out.trigger();
 
-      module->sample_players[module->selected_sample_index].loadSample(filename);
+			/*
+			module->sample_players[module->selected_sample_index].loadSample(filename);
 			module->loaded_filenames[module->selected_sample_index] = module->sample_players[module->selected_sample_index].getFilename();
-      module->updateSampleRateDivision(); 
+			module->updateSampleRateDivision();
 			module->setRoot(filename);
+			*/
+			module->sample_players[sample_number].loadSample(filename);
+			module->loaded_filenames[sample_number] = module->sample_players[sample_number].getFilename();
+			module->updateSampleRateDivision();
+			module->setRoot(filename);		
 		}
 	}
 };
