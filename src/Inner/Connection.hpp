5143d504b682125999fd7886c4fb51ff5bd11628
Bret Truchan <btruchan@graphiclanguage.net>
2023-06-08 20:04:44 -0700

    It appears that I have macro bridging working!

--- src/Inner/Connection.hpp
+++ src/Inner/Connection.hpp
@@ -7,16 +7,21 @@ public:
     unsigned int source_port_index;
     std::string destination_module_uuid;
     unsigned int destination_port_index;
+    std::string uuid = "";
 
     Connection(
         std::string src_mod_uuid, 
         unsigned int src_port_idx, 
         std::string dest_mod_uuid, 
-        unsigned int dest_port_idx
+        unsigned int dest_port_idx,
+        std::string uuid
     ) : 
     source_module_uuid(src_mod_uuid), 
     source_port_index(src_port_idx), 
     destination_module_uuid(dest_mod_uuid), 
-    destination_port_index(dest_port_idx) 
-    {}
+    destination_port_index(dest_port_idx),
+    uuid(uuid)
+    {
+
+    }
 };
