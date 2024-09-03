7800f22733750e84d17d6c4d3e6dca487a1542f4
Bret Truchan <clone45@gmail.com>
2024-09-03 12:09:12 -0700

    Completed grain engine expander conversion

--- src/GrainEngineMK2Expander/GrainEngineMK2ExpanderWidget.hpp
+++ src/GrainEngineMK2Expander/GrainEngineMK2ExpanderWidget.hpp
@@ -4,29 +4,26 @@ struct GrainEngineMK2ExpanderWidget : ModuleWidget
     {
         setModule(module);
 
-        // Load and apply theme
-        // theme.load("grain_engine_mk2_expander");
-        // applyTheme();
-
-        setPanel(createPanel(
+        PanelHelper panelHelper(this);
+        panelHelper.loadPanel(
             asset::plugin(pluginInstance, "res/grain_engine_mk2_expander/grain_engine_mk2_expander_panel.svg"),
             asset::plugin(pluginInstance, "res/grain_engine_mk2_expander/grain_engine_mk2_expander_panel-dark.svg")
-        ));
+        );
 
-        addInput(createInputCentered<VoxglitchInputPort>(Vec(40.417305, 59.081532), module, GrainEngineMK2Expander::AUDIO_IN_LEFT));
-        addInput(createInputCentered<VoxglitchInputPort>(Vec(50.382683, 96.631500), module, GrainEngineMK2Expander::AUDIO_IN_RIGHT));
+        addInput(createInputCentered<VoxglitchInputPort>(panelHelper.findNamed("left_input"), module, GrainEngineMK2Expander::AUDIO_IN_LEFT));
+        addInput(createInputCentered<VoxglitchInputPort>(panelHelper.findNamed("right_input"), module, GrainEngineMK2Expander::AUDIO_IN_RIGHT));
 
-        addInput(createInputCentered<VoxglitchInputPort>(Vec(25.117329, 176.798842), module, GrainEngineMK2Expander::RECORD_START_INPUT));
-        addParam(createLightParamCentered<VCVLightBezel<WhiteLight>>(Vec(25.117329, 207.750000), module, GrainEngineMK2Expander::RECORD_START_BUTTON_PARAM, GrainEngineMK2Expander::RECORDING_LIGHT));
+        addInput(createInputCentered<VoxglitchInputPort>(panelHelper.findNamed("start_input"), module, GrainEngineMK2Expander::RECORD_START_INPUT));
+        addParam(createLightParamCentered<VCVLightBezel<WhiteLight>>(panelHelper.findNamed("start_button"), module, GrainEngineMK2Expander::RECORD_START_BUTTON_PARAM, GrainEngineMK2Expander::RECORDING_LIGHT));
 
-        addInput(createInputCentered<VoxglitchInputPort>(Vec(65.267303, 178.482361), module, GrainEngineMK2Expander::RECORD_STOP_INPUT));
-        addParam(createLightParamCentered<VCVLightBezel<WhiteLight>>(Vec(65.250000, 207.750000), module, GrainEngineMK2Expander::RECORD_STOP_BUTTON_PARAM, GrainEngineMK2Expander::STOPPED_LIGHT));
+        addInput(createInputCentered<VoxglitchInputPort>(panelHelper.findNamed("stop_input"), module, GrainEngineMK2Expander::RECORD_STOP_INPUT));
+        addParam(createLightParamCentered<VCVLightBezel<WhiteLight>>(panelHelper.findNamed("stop_button"), module, GrainEngineMK2Expander::RECORD_STOP_BUTTON_PARAM, GrainEngineMK2Expander::STOPPED_LIGHT));
 
-        addInput(createInputCentered<VoxglitchInputPort>(Vec(25.067327, 260.582733), module, GrainEngineMK2Expander::SAMPLE_SLOT_INPUT));
-        addParam(createParamCentered<Trimpot>(Vec(65.332687, 261.082764), module, GrainEngineMK2Expander::SAMPLE_SLOT_KNOB_PARAM));
+        addInput(createInputCentered<VoxglitchInputPort>(panelHelper.findNamed("sample_input"), module, GrainEngineMK2Expander::SAMPLE_SLOT_INPUT));
+        addParam(createParamCentered<Trimpot>(panelHelper.findNamed("sample_attn_knob"), module, GrainEngineMK2Expander::SAMPLE_SLOT_KNOB_PARAM));
 
-        addOutput(createOutputCentered<VoxglitchOutputPort>(Vec(24.917328, 342.437317), module, GrainEngineMK2Expander::PASSTHROUGH_LEFT));
-        addOutput(createOutputCentered<VoxglitchOutputPort>(Vec(65.082687, 342.437317), module, GrainEngineMK2Expander::PASSTHROUGH_RIGHT));
+        addOutput(createOutputCentered<VoxglitchOutputPort>(panelHelper.findNamed("left_output"), module, GrainEngineMK2Expander::PASSTHROUGH_LEFT));
+        addOutput(createOutputCentered<VoxglitchOutputPort>(panelHelper.findNamed("right_output"), module, GrainEngineMK2Expander::PASSTHROUGH_RIGHT));
     }
 
     void appendContextMenu(Menu *menu) override
