e81bdb889cd91aae211940f7caf7b78bee897dc3
Bret Truchan <clone45@gmail.com>
2024-11-09 18:48:35 -0800

    Updated version to 2.31.0 for release of satanonaut blank panel

--- src/SatanonautUnearthed/SatanonautUnearthedWidget.hpp
+++ src/SatanonautUnearthed/SatanonautUnearthedWidget.hpp
@@ -7,7 +7,7 @@ struct SatanonautUnearthedWidget : ModuleWidget
         PanelHelper panelHelper(this);
         panelHelper.loadPanel(
             asset::plugin(pluginInstance, "res/satanonaut_unearthed/satanonaut_unearthed_panel.svg"),
-            asset::plugin(pluginInstance, "res/satanonaut_unearthed/satanonaut_unearthed_panel-dark.svg")
+            asset::plugin(pluginInstance, "res/satanonaut_unearthed/satanonaut_unearthed_panel.svg")
         );
     }
 };
