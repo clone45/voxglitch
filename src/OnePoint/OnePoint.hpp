f6ba9a5c489c9d3aeb6baccced9651e0ca61be43
Bret Truchan <clone45@gmail.com>
2025-04-06 10:14:17 -0700

    OnePoint and OneZero will now auto-save the selected sequence (#254)

--- src/OnePoint/OnePoint.hpp
+++ src/OnePoint/OnePoint.hpp
@@ -83,6 +83,7 @@ struct OnePoint : VoxglitchModule
     {
         json_t *json_root = json_object();
         json_object_set_new(json_root, "path", json_string(path.c_str()));
+        json_object_set_new(json_root, "selected_sequence", json_integer(selected_sequence));
         return json_root;
     }
 
@@ -94,6 +95,16 @@ struct OnePoint : VoxglitchModule
         {
             this->path = json_string_value(loaded_path_json);
             this->loadData(this->path);
+            
+            // Restore the selected sequence if it exists in the saved data
+            json_t *loaded_sequence_json = json_object_get(json_root, "selected_sequence");
+            if (loaded_sequence_json)
+            {
+                unsigned int loaded_sequence = json_integer_value(loaded_sequence_json);
+                // Make sure the loaded sequence is valid
+                if (loaded_sequence < sequences.size())
+                    this->selectNewSequence(loaded_sequence);
+            }
         }
     }
 
@@ -245,7 +256,7 @@ struct OnePoint : VoxglitchModule
     {
         std::ifstream input_file(path);
 
-        selected_sequence = 0;
+        // Note: We don't set selected_sequence = 0 here anymore as it will be set in dataFromJson if available
 
         // Clear out existing data
         sequences.clear();
